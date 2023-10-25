e15151e1242819ca16a6c03e89e277eaeb375ab8
PetroBrodetskyi <petro.brodetskyi@gmail.com>
2023-10-25 20:58:42 +0200

    stylesStructureChanged

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,31 +1,31 @@
 import React from 'react';
-import "./Profile.css"
+import css from"./Profile.module.css"
 
 const Profile = ({ username, tag, location, avatar, stats }) => (
-  <div className="profile">
-    <div className="description">
+  <div className={css.profile}>
+    <div className={css.description}>
       <img
         src={avatar}
         alt="User avatar"
-        className="avatar"
+        className={css.avatar}
       />
-      <p className="name">{username}</p>
-      <p className="tag">@{tag}</p>
-      <p className="location">{location}</p>
+      <p className={css.name}>{username}</p>
+      <p className={css.tag}>@{tag}</p>
+      <p className={css.location}>{location}</p>
     </div>
 
-    <ul className="stats">
-      <li className="stats-flex">
-        <span className="label">Followers</span>
-        <span className="quantity">{stats.followers}</span>
+    <ul className={css.stats}>
+      <li className={css.statsflex}>
+        <span className={css.label}>Followers</span>
+        <span className={css.quantity}>{stats.followers}</span>
       </li>
-      <li className="stats-flex">
-        <span className="label">Views</span>
-        <span className="quantity">{stats.views}</span>
+      <li className={css.statsflex}>
+        <span className={css.label}>Views</span>
+        <span className={css.quantity}>{stats.views}</span>
       </li>
-      <li className="stats-flex">
-        <span className="label">Likes</span>
-        <span className="quantity">{stats.likes}</span>
+      <li className={css.statsflex}>
+        <span className={css.label}>Likes</span>
+        <span className={css.quantity}>{stats.likes}</span>
       </li>
     </ul>
   </div>
