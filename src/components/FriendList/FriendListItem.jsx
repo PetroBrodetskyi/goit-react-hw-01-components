2b8a8e70383fbb70c8bdc424327d95a35ca9f3c5
PetroBrodetskyi <petro.brodetskyi@gmail.com>
2023-10-28 23:21:53 +0200

    iconsAdd

--- src/components/FriendList/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import css from "./FriendListItem.module.css"
+import { AiFillHeart, AiFillPushpin, AiFillCloseCircle } from "react-icons/ai";
+
 
 const FriendListItem = ({ avatar, name, isOnline }) => (
   <li className={css.frienditem}>
+    <div className={css.nav}>
     <span className={`${css.status} ${isOnline ? css.online : css.offline}`}></span>
     <img
       className={css.avatar}
@@ -10,7 +13,13 @@ const FriendListItem = ({ avatar, name, isOnline }) => (
       alt="User avatar"
       width="48"
     />
-    <p className={css.friendname}>{name}</p>
+      <p className={css.friendname}>{name}</p>
+    </div>
+    <div className={css.icons}>
+    <AiFillHeart className={css.iconheart}/>
+    <AiFillPushpin className={css.pin}/>
+    <AiFillCloseCircle className={css.circle}/>
+    </div>
   </li>
 );
 
